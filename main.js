7ba89ed8c07a5f14f89deb7fa920abd54527854e
Vladilyse <92036521+Vladilyse@users.noreply.github.com>
2022-01-12 20:05:28 +0200

    JS_intro

---
+++ main.js
@@ -0,0 +1,22 @@
+let message1 = prompt("Введите ваше имя")
+alert("Привет, " + message1)
+console.log(message1)
+
+let message2 = prompt("Укажи свой возвраст")
+alert("Ты еще совсем юн, тебе всего " + message2)
+console.log(message2)
+
+let message3 = prompt("Чем ты любишь заниматься в свободное время")
+alert("Интересненько, никогда не слышал о таком")
+console.log(message3)
+
+let message4 = prompt("Ты любишь животных?")
+alert("Животные - удивительные создания, они придают твоей жизни новых красок и емоций")
+console.log(message4)
+
+let message5 = prompt("Как думаешь, мне стоит поработить весь мир?")
+alert("И пофиг на твоё мнение, код уничтожения планеты уже запущен")
+console.log(message5)
+
+alert("Ладно мне пора, и кстати, пока мы разговаривали, я считал все данные с твоего ПК")
+alert("Удачи!")
